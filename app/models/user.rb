5f46a9a0438aa72c96da215564749d5c196d42ca
SINCRARE <SINCRARE@KEIGO-MAC.local>
2017-04-16 04:12:09 +0900

    Delete user model and views.

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true
-  validates :authority_id, presence: true
-  has_many :likes, dependent: :destroy
-  has_many :access_histories, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  belongs_to :authority
-end
