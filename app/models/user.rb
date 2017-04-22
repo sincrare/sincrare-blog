6aa85129eb2b70bacba107a78e543aeaf77c067a
SINCRARE <SINCRARE@KEIGO-MAC.local>
2017-04-23 02:22:53 +0900

    Add devise original params(name, authority_id, comment).

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+  validates :name, presence: true
+  validates :authority_id, presence: true
+  has_many :likes
+  has_many :access_histories
+  has_many :comments
+  belongs_to :authority
 end
