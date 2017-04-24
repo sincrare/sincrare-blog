35c23227f03e8553e6463f367ab06064ecf59a24
SINCRARE <SINCRARE@KEIGO-MAC.local>
2017-04-25 02:15:23 +0900

    Update devise.

--- db/migrate/20170421162605_add_param_to_users.rb
+++ db/migrate/20170421162605_add_param_to_users.rb
@@ -1,7 +1,7 @@
 class AddParamToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :name, :string
-    add_column :users, :authority_id, :integer, default: 1, null: false
+    add_column :users, :authority_id, :integer
     add_column :users, :comment, :text
   end
 end
