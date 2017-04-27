9691e44f45b25a3518fc53b07a067999321681ee
SINCRARE <SINCRARE@KEIGO-MAC.local>
2017-04-27 23:49:39 +0900

    Add article search command.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,44 +1,90 @@
 class ArticlesController < ApplicationController
+  before_action :set_authority
   ARTICLES_COUNT_PER_PAGE = 5
 
   # GET /articles
   def index
-    @articles = Article.includes(:authorities).where(authorities: {id: 1}).take(ARTICLES_COUNT_PER_PAGE)
+    @articles = Article.includes(:authorities).order(entry_at: :desc).where(authorities: {id: @authorityId}, is_draft: false).take(ARTICLES_COUNT_PER_PAGE)
   end
 
   # GET /articles/1
   def show
-    @article = Article.find(params[:id])
+    @article = Article.includes(:authorities).find_by(id: params[:id], authorities: {id: @authorityId}, is_draft: false)
   end
 
   # GET /articles/page/1
   def page
-    @articles = Article.where('id = 1')
+    @articles = Article.includes(:authorities).order(entry_at: :desc).where(authorities: {id: @authorityId}, is_draft: false).offset(current_page * ARTICLES_COUNT_PER_PAGE).take(ARTICLES_COUNT_PER_PAGE)
     render 'index'
   end
 
-  # GET /articles/daily/2017/01/01
+  # GET /articles/daily/2017/01/01/page/1
   def daily
-
+    targetDay = datetime_parse(params[:year], params[:month], params[:day])
+    @articles = Article.includes(:authorities).order(entry_at: :desc).where(entry_at: targetDay.to_time.all_day , authorities: {id: @authorityId}, is_draft: false).offset(current_page * ARTICLES_COUNT_PER_PAGE).take(ARTICLES_COUNT_PER_PAGE)
     render 'index'
   end
 
-  # GET /articles/monthly/2017/01
+  # GET /articles/monthly/2017/01/page/1
   def monthly
-
+    targetMonth = datetime_parse(params[:year], params[:month])
+    @articles = Article.includes(:authorities).order(entry_at: :desc).where(entry_at: targetMonth.to_time.all_month , authorities: {id: @authorityId}, is_draft: false).offset(current_page * ARTICLES_COUNT_PER_PAGE).take(ARTICLES_COUNT_PER_PAGE)
     render 'index'
   end
 
-  # GET /articles/search_by/keyword
+  # GET /articles/search_by/keyword/page/1
   def search_by
-
+    @articles = Article.includes(:authorities).order(entry_at: :desc).where(authorities: {id: @authorityId}, is_draft: false).where('content like ? or title like ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%").offset(current_page * ARTICLES_COUNT_PER_PAGE).take(ARTICLES_COUNT_PER_PAGE)
     render 'index'
   end
 
-  # GET /articles/category_by/1
-  def category_by
-
-    render 'index'
+  # GET /articles/tag/1/page/1
+  def tag
+    @articles = Article.includes([:authorities, :tags]).order(entry_at: :desc).where(tags: {id: params[:tag_id]}, authorities: {id: @authorityId}, is_draft: false).offset(page * ARTICLES_COUNT_PER_PAGE).take(ARTICLES_COUNT_PER_PAGE)
+    #render 'index'
   end
 
+  # POST /articles/1/entry_comment
+  def entry_comment
+    @article = Article.includes(:authorities).find_by(id: params[:id], authorities: {id: @authorityId}, is_draft: false)
+    if @article.present?
+      @comment = Comment.new(article_id: @article.id, content: comment_params[:content], entry_user_id: @authorityId)
+      if @comment.save
+        redirect_to article_path(@article, anchor: :comments), notice: 'コメントを投稿しました。'
+      else
+        redirect_to article_path(@article, anchor: :comments), notice: 'コメントの投稿に失敗しました。'
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_authority
+      # 未ログインは権限1
+      @authorityId = 1
+      if user_signed_in?
+        @authorityId = current_user.authority_id
+      end
+    end
+
+    def current_page
+      page = 0
+      if params[:page_id].present?
+        page = params[:page_id].to_i
+      end
+      return page
+    end
+
+    def datetime_parse(year, month = '1', day = '1')
+      date = nil
+      if year.present? && month.present? && day.present?
+        date = DateTime.parse(year.rjust(4, '0') + month.rjust(2, '0') + day.rjust(2, '0'))
+      end
+      return date
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,18 @@ Rails.application.routes.draw do
 
   resources :articles, :only => [:index, :show] do
     collection do
-      get 'page/:id', to: 'articles#page'
+      get 'page/:page_id', to: 'articles#page'
       get 'daily/:year/:month/:day', to: 'articles#daily'
+      get 'daily/:year/:month/:day/page/:page_id', to: 'articles#daily'
       get 'monthly/:year/:month', to: 'articles#monthly'
+      get 'monthly/:year/:month/page/:page_id', to: 'articles#monthly'
       get 'search_by/:keyword', to: 'articles#search_by'
-      get 'category_by/:category_id', to: 'articles#category_by'
+      get 'search_by/:keyword/page/:page_id', to: 'articles#search_by'
+      get 'tag/:tag_id', to: 'articles#tag'
+      get 'tag/:tag_id/page/:page_id', to: 'articles#tag'
+    end
+    member do
+      post 'entry_comment'
     end
   end
   namespace :admin do
