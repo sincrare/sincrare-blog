b693fe5aa5ece94a9ca3a3205168d2b3a79c570c
SINCRARE <SINCRARE@KEIGO-MAC.local>
2017-04-30 05:12:09 +0900

    Add DB seed(Authority, Tag).

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,14 @@
 # coding: utf-8
 Authority.create(name: '未ログイン')
+Authority.create(name: '一般')
+Authority.create(name: '友達')
+Authority.create(name: '親族')
+
+Tag.create(name: 'サイト紹介')
+Tag.create(name: '詩、文章')
+Tag.create(name: '目指せ100冊読破!')
+Tag.create(name: '木工')
+Tag.create(name: '旅行')
+Tag.create(name: '子ども関連')
+Tag.create(name: '畑')
+Tag.create(name: 'IT')
